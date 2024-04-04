e86af6859a2b2a6603ed36d1b1c46b7acf5a86e6
Riley Burke <rjb489@nau.edu>
2024-04-04 15:21:52 -0700

    Added empty source files

---
+++ src/chat_node.h
@@ -0,0 +1,33 @@
+#ifndef chat_node_h
+#define chat_node_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+// ChatNode
+typedef struct chat_node_struct
+   {
+    unsigned int ip;
+    unsigned short int port;
+    char name[16];
+   } ChatNode;
+
+// ChatNodeListElement
+typedef struct chat_node_list_element_struct
+   {
+    ChatNode chat_node;
+    struct chat_node_list_element_struct* next;
+   } ChatNodeListElement;
+
+// ChatNode List
+typedef struct chat_node_list_element_struct
+   {
+    ChatNodeListElement* first;
+    ChatNodeListElement* last;
+   } ChatNodeList;
+
+
+
+#endif
---
+++ src/message.h
@@ -0,0 +1,27 @@
+#ifndef message_h
+#define message_h
+
+#include "chat_node.h"
+
+// Message Type Identifiers
+#define JOIN         1
+#define LEAVE        2
+#define NOTE         3
+#define SHUTDOWN     4
+#define SHUTDOWN_ALL 5
+#define JOINING      6
+#define LEFT         7
+
+
+// Structure Definitions
+
+typedef char Note[64];
+
+typedef struct message_struct
+   {
+    unsigned char type;
+    ChatNode chat_node;
+    Note note;
+   } Message;
+
+#endif
