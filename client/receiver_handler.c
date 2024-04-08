abac26aef7fa94a58e8a4efb02f93c03f829caa5
chd63 <chd63@nau.edu>
2024-04-07 17:56:14 -0700

    added sudocode to sender,receiver

--- client/receiver_handler.c
+++ client/receiver_handler.c
@@ -10,11 +10,32 @@
 // server loop function called receiveThread gets a Proprties pointer passed in
 
     // server loop
+     
+        // will wait for a responce from the server
+
+        // will call the server connecting function(switch statment function)
 
 
 
 // server connecting function (called in server loop)
 
     // switch statment 
+    // we need to check what type of command is put in and 
+    // see what we get from the server
 
-    // CASE 1: 
+    // CASE 1: NOTE
+
+        // just display the NOTE to the user
+
+    // CASE 2: LEAVE
+
+        // display someone has left the server
+
+    // CASE 3: JOIN 
+
+        // display someone has joined the server
+
+    // CASE 4: SHUTDOWN ALL 
+
+        // disaply someone has shutdown the whole server 
+        // shutdown the client
